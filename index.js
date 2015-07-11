b3d5eb159a31f53acf766d26ddddddf14cd7577c
Hugi Hlynsson <hugihlynsson@gmail.com>
2015-07-11 19:16:59 +0000

    Add logic to find new offers

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const fetchData = require('./server/fetchData');
 const extractOffers = require('./server/extractOffers');
+const findNewOffers = require('./server/findNewOffers');
 const storeResults = require('./server/storeResults');
 
 fetchData()
   .then(extractOffers)
+  .then(findNewOffers)
   .then(storeResults);
