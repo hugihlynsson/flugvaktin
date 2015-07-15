38a0832b039b496c971bd64b27f6190e4df76a32
Hugi Hlynsson <hugihlynsson@gmail.com>
2015-07-15 10:56:50 +0000

    Get data periodically and add stack trace to error debugging

--- index.js
+++ index.js
@@ -7,15 +7,21 @@ const findNewOffers = require('./server/findNewOffers');
 const storeResults = require('./server/storeResults');
 const emailNewOffers = require('./server/emailNewOffers');
 
-fetchData()
-  .then(extractOffers)
-  .then(appendLastOffers)
-  .then(findNewOffers)
-  .then(emailNewOffers)
-  .then(storeResults)
-  .then(function(offers) {
-    console.log('Updated data successfully, new offers: ', offers.new.length);
-  })
-  .catch(function(err) {
-    console.log('Updating data failed', err);
-  });
+(function circle() {
+  fetchData()
+    .then(extractOffers)
+    .then(appendLastOffers)
+    .then(findNewOffers)
+    .then(emailNewOffers)
+    .then(storeResults)
+    .then(function(offers) {
+      console.log('Updated data successfully, new offers:', offers.new.length);
+    })
+    .catch(function(err) {
+      console.log('Updating data failed');
+      console.trace(err.stack);
+    })
+    .then(function() {
+      setTimeout(circle, Math.round(1000 * 60 * 4 + Math.random() * 60 * 1000));
+    });
+})();
