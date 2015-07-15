93fc68f6e52fbaabde7d14494f6bbdf74128c30b
Hugi Hlynsson <hugihlynsson@gmail.com>
2015-07-15 09:56:51 +0000

    Send email with new offers

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const extractOffers = require('./server/extractOffers');
 const appendLastOffers = require('./server/appendLastOffers');
 const findNewOffers = require('./server/findNewOffers');
 const storeResults = require('./server/storeResults');
+const emailNewOffers = require('./server/emailNewOffers');
 
 fetchData()
   .then(extractOffers)
   .then(appendLastOffers)
   .then(findNewOffers)
+  .then(emailNewOffers)
   .then(storeResults)
   .then(function(offers) {
     console.log('Updated data successfully, new offers: ', offers.new.length);
---
+++ server/emailNewOffers.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: process.env.FLUGVAKTIN_EMAIL,
+    pass: process.env.FLUGVAKTIN_PASS,
+  },
+});
+
+let mailOptions = {
+  from: `Flugvaktin <${process.env.FLUGVAKTIN_EMAIL}>`,
+  to: 'hugihlynsson@gmail.com',
+  subject: 'Nýtt netverð frá Akureyri til Reykjavíkur',
+};
+
+module.exports = function emailNewOffers(offers) {
+  if (offers.new.length) {
+    mailOptions.text = offers.new.map(function(offer) {
+      return `
+        ${offer.route}
+        ${offer.date} kl. ${offer.time} fyrir ${offer.price}
+        https://www.flugfelag.is${offer.link}
+      `;
+    }).join('');
+    return new Promise(function(resolve, reject) {
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          reject(new Error(error));
+        }
+        console.log('Send mail', info);
+        resolve(offers);
+      });
+    });
+  }
+};
